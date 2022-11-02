c288ced67af1922296e919e4c89fb70e8dcb6b91
Hoang Long <80147780+coder6622@users.noreply.github.com>
2022-11-02 21:33:11 +0700

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,24 @@ import 'package:note_app/resources/constants/string_constant.dart';
 import 'package:note_app/screens/home.screen.dart';
 import 'package:note_app/utils/routes/routes.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ScreenUtilInit(
-      builder: ((context, child) => MaterialApp(
-            title: AppString.instance.nameApp,
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-              fontFamily: 'Lato',
-            ),
-            routes: Routes.routes,
-            home: const HomeScreen(),
-          )),
-      designSize: const Size(428, 926),
+    MultiProvider(
+      providers: const [],
+      child: ScreenUtilInit(
+        builder: ((context, child) => MaterialApp(
+              title: AppString.instance.nameApp,
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                fontFamily: 'Lato',
+              ),
+              routes: Routes.routes,
+              home: const HomeScreen(),
+            )),
+        designSize: const Size(428, 926),
+      ),
     ),
   );
 }
