8adb7b358616a3c4717508a029a5f4ffb89ba282
dominhdang148 <myclone235@gmail.com>
2022-11-14 00:28:33 +0700

    Add Fo0der grid item ⚠ (haven't done yet)

--- lib/resources/constants/asset_path.dart
+++ lib/resources/constants/asset_path.dart
@@ -5,4 +5,8 @@ class AssetPaths{
   static const String google='$prePath/ic_google.png';
   static const String eyeShow='$prePath/ic_show.png';
   static const String eyeHide='$prePath/ic_hide.png';
+  static const String folderLocked='$prePath/locked_folder_sample.png';
+  static const String folderUnlocked='$prePath/unlocked_folder_sample.png';
+  static const String testLocked='$prePath/unlocked_folder.svg';
+  static const String showMore='$prePath/more.png';
 }
--- lib/screens/folders_page.dart
+++ lib/screens/folders_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note_app/widgets/app_bar.dart';
+import 'package:note_app/widgets/folders/folders.dart';
 
 class FolderScreen extends StatefulWidget {
   const FolderScreen({super.key});
@@ -13,11 +15,20 @@ class _FolderScreenState extends State<FolderScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppbar(
-        title: "UniNote",
         handleBackBtn: () {},
+        title: "UniNote",
         isBackBtn: false,
       ),
-      
+      body: Center(
+        child: Column(
+          children:  [
+            Padding(
+              padding: EdgeInsets.all(8.w),
+              child: const FolderGrid(isLocked: false,title: "All note",numberOfNote: 3,),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
