15cdd963538f22ff1a4f6dac9f3d0040df018a80
dominhdang148 <myclone235@gmail.com>
2022-12-15 13:43:39 +0700

    Hoàn thiện trang User Info ✅, bổ sung ImagePicker ⚠(Chưa hoạt động)

--- lib/screens/info_user.screen.dart
+++ lib/screens/info_user.screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:note_app/resources/colors/colors.dart';
 import 'package:note_app/resources/constants/asset_path.dart';
 import 'package:note_app/resources/fonts/enum_text_styles.dart';
@@ -8,17 +9,31 @@ import 'package:note_app/resources/fonts/text_styles.dart';
 import 'package:note_app/screens/user_info/buttons.dart';
 import 'package:note_app/widgets/app_bar.dart';
 import 'package:note_app/widgets/avatar/avatar_appbar.dart';
+import 'package:note_app/widgets/text_field/text_field.dart';
 import 'package:provider/provider.dart';
-
 import '../providers/auth.provider.dart';
 
 class InfoUserScreen extends StatelessWidget {
   const InfoUserScreen({super.key});
 
+  _getFromGallery() async {
+    XFile? file = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (file != null) {
+      // File imageFile = File(file.path);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     UserProvider userProvider = Provider.of<UserProvider>(context);
+    TextEditingController usernameController = TextEditingController();
+    TextEditingController emailController = TextEditingController();
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: CustomAppbar(
         title: "User info",
         handleBackBtn: (() => Navigator.of(context).pop()),
@@ -27,7 +42,8 @@ class InfoUserScreen extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 32.h),
           child: Center(
-            child: Column(children: [
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Padding(
                 padding: EdgeInsets.all(16.w),
                 child: Row(
@@ -56,7 +72,9 @@ class InfoUserScreen extends StatelessWidget {
                             mainAxisAlignment: MainAxisAlignment.start,
                             children: [
                               SmallIconButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  _getFromGallery();
+                                },
                                 btnText: "Upload",
                                 svgIcon: SvgPicture.asset(AssetPaths.upload),
                               ),
@@ -87,15 +105,40 @@ class InfoUserScreen extends StatelessWidget {
               SizedBox(
                 height: 32.h,
               ),
-              Text(userProvider.getCurrentUser.displayName!,
-                  style: AppTextStyles.h4[TextWeights.semibold]),
-              Text(userProvider.getCurrentUser.email!,
-                  style: AppTextStyles.h6[TextWeights.semibold]),
-              Text(userProvider.getCurrentUser.uID!,
-                  style: AppTextStyles.h6[TextWeights.semibold]),
-              SizedBox(
-                height: 100.h,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 4.h),
+                child: Text(
+                  "User name",
+                  style: AppTextStyles.h5[TextWeights.semibold]
+                      ?.copyWith(color: AppColors.primary),
+                ),
               ),
+              InputField(
+                controller: usernameController,
+                hintText: userProvider.getCurrentUser.displayName!,
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 4.h),
+                child: Text(
+                  "Email",
+                  style: AppTextStyles.h5[TextWeights.semibold]
+                      ?.copyWith(color: AppColors.primary),
+                ),
+              ),
+              InputField(
+                controller: emailController,
+                hintText: userProvider.getCurrentUser.email!,
+              ),
+              // Text(userProvider.getCurrentUser.displayName!,
+              //     style: AppTextStyles.h4[TextWeights.semibold]),
+              // Text(userProvider.getCurrentUser.email!,
+              //     style: AppTextStyles.h6[TextWeights.semibold]),
+              // Text(userProvider.getCurrentUser.uID!,
+              //     style: AppTextStyles.h6[TextWeights.semibold]),
+              // SizedBox(
+              //   height: 100.h,
+              // ),
               ColorButton(
                 text: 'Change my password',
                 color: const Color(0xff8278F8),
